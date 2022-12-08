e93ed4ee9d877dfea45b998110b1073082f60e91
Paulo Moutinho <paulo_vicali@icloud.com>
2022-12-08 01:03:55 -0300

    refactor: deleta pasta não utilizada

--- src/components/Link/PageLink.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react"
-import { PageLink } from "./PageLink"
-
-test("active link renders correctly", () => {
-    // Arrange
-    const { debug } = render(<PageLink href="/" />)
-
-    debug()
-})
