32749e4040581d638ce5a6cb274ffe45f9497656
Paulo Moutinho <paulo_vicali@icloud.com>
2022-12-12 01:38:05 -0300

    feat: adiciona tipagem para requisicão

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,7 +1,17 @@
+// --------> post typing <--------
 export interface FormData {
     name: string
     email: string;
     password: string;
     privacyTerms: boolean
     profession: string;
+}
+
+// --------> get typing <--------
+
+export interface ListProps {
+    name: string 
+    email: string 
+    password: string 
+    profession: string 
 }
