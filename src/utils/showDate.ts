0f92a26d1f5c9e7b4b2de9da859583c98f6532e9
Paulo Moutinho Vitor <paulo.vitor@viavarejo.com>
2022-12-28 22:40:35 -0300

    style: formatação

--- src/utils/showDate.ts
+++ src/utils/showDate.ts
@@ -1,12 +1,12 @@
 export let createdAt = () => {
-    const date = new Date()
-    const showDate = date.toLocaleDateString()
-    const H = formatTime(date.getHours())
-    const M = formatTime(date.getMinutes())
+  const date = new Date();
+  const showDate = date.toLocaleDateString();
+  const H = formatTime(date.getHours());
+  const M = formatTime(date.getMinutes());
 
-    function formatTime(time: number) {
-      return String(time).padStart(2, '0')
-    }
-
-    return `${showDate} - ${H}:${M}`
+  function formatTime(time: number) {
+    return String(time).padStart(2, "0");
   }
+
+  return `${showDate} - ${H}:${M}`;
+};
