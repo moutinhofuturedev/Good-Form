89a87b65db5b90588a6a55c889548ce29f95804c
Paulo Moutinho Vitor <paulo.vitor@viavarejo.com>
2022-12-28 22:38:13 -0300

    style: formatação da página de formulário

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -7,148 +7,154 @@ import { api } from "../api/api";
 import { createdAt } from '../utils/showDate';
 
 export function Form() {
-    const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<FormData>()
-    const toast = useToast()
-    const router = useRouter()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<FormData>();
+  const toast = useToast();
+  const router = useRouter();
 
-    const OnSubmit: SubmitHandler<FormData> = async (data) => {
-        try {
-          await new Promise(resolve => setTimeout(() => {
-            createdAt()
-              resolve(
-                  api.post("/form", {
-                      name: data.name,
-                      email: data.email,
-                      password: data.password,
-                      privacyTerms: data.privacyTerms,
-                      profession: data.profession,
-                      createdAt: createdAt()
-                  })
-              )
-          }, 5000))
-          toast({
-            title: 'Conta criada.',
-            description: "Cadastro realizado com sucesso.",
-            status: 'success',
-            duration: 6000,
-            isClosable: true,
-            position:  "top-left"
-          })
-  
-          router.push("/list")
-        } catch (error) {
-          toast({
-            title: 'Erro de cadastro',
-            status: "error",
-            description: 'Não foi possível fazer novo registro',
-            duration: 6000,
-            isClosable: true,
-            position: "top-left"
-          })
-        }
+  const OnSubmit: SubmitHandler<FormData> = async (data) => {
+    try {
+      await new Promise((resolve) =>
+        setTimeout(() => {
+          createdAt();
+          resolve(
+            api.post("/form", {
+              name: data.name,
+              email: data.email,
+              password: data.password,
+              privacyTerms: data.privacyTerms,
+              profession: data.profession,
+              createdAt: createdAt(),
+            })
+          );
+        }, 5000)
+      );
+      toast({
+        title: "Conta criada.",
+        description: "Cadastro realizado com sucesso.",
+        status: "success",
+        duration: 6000,
+        isClosable: true,
+        position: "top-left",
+      });
+
+      router.push("/list");
+    } catch (error) {
+      toast({
+        title: "Erro de cadastro",
+        status: "error",
+        description: "Não foi possível fazer novo registro",
+        duration: 6000,
+        isClosable: true,
+        position: "top-left",
+      });
     }
+  };
 
-    return (
-      <Flex
-        height="100%"
-        flexDirection="column"
-        justifyContent="center"
-        align="center"
-        px="2rem"
-      >
-        <Heading as="h2" mb="2rem" fontSize={["lg", "2xl"]}>
-          Formulário React Hook Form
-        </Heading>
-        <FormControl minW={["15rem", "20rem"]}>
-          <FormLabel>Name</FormLabel>
-          <Input
-            type="text"
-            id="name"
-            placeholder="Seu nome"
-            {...register("name", { required: true })}
-          />
-          {errors.name?.type === "required" && (
-            <Text color="red.500" fontSize="sm">
-              Este campo é obrigatório
-            </Text>
-          )}
+  return (
+    <Flex
+      height="100%"
+      flexDirection="column"
+      justifyContent="center"
+      align="center"
+      px="2rem"
+    >
+      <Heading as="h2" mb="2rem" fontSize={["lg", "2xl"]}>
+        Formulário React Hook Form
+      </Heading>
+      <FormControl minW={["15rem", "20rem"]}>
+        <FormLabel>Name</FormLabel>
+        <Input
+          type="text"
+          id="name"
+          placeholder="Seu nome"
+          {...register("name", { required: true })}
+        />
+        {errors.name?.type === "required" && (
+          <Text color="red.500" fontSize="sm">
+            Este campo é obrigatório
+          </Text>
+        )}
 
-          <FormLabel mt="1rem">E-mail</FormLabel>
-          <Input
-            type="email"
-            id="email"
-            placeholder="Seu e-mail"
-            {...register("email", { required: true })}
-          />
-          {errors.email?.type === "required" && (
-            <Text color="red.500" fontSize="sm">
-              Este campo é obrigatório
-            </Text>
-          )}
+        <FormLabel mt="1rem">E-mail</FormLabel>
+        <Input
+          type="email"
+          id="email"
+          placeholder="Seu e-mail"
+          {...register("email", { required: true })}
+        />
+        {errors.email?.type === "required" && (
+          <Text color="red.500" fontSize="sm">
+            Este campo é obrigatório
+          </Text>
+        )}
 
-          <FormLabel mt="1rem">Senha</FormLabel>
-          <Input
-            type="password"
-            id="password"
-            placeholder="Sua senha"
-            {...register("password", { required: true, minLength: 10 })}
-          />
-          {errors.password?.type === "required" && (
-            <Text color="red.500" fontSize="sm">
-              Este campo é obrigatório
-            </Text>
-          )}
-          {errors.password?.type === "minLength" && (
-            <Text color="red.500" fontSize="sm">
-              Senha deve conter 10 caracteres
-            </Text>
-          )}
+        <FormLabel mt="1rem">Senha</FormLabel>
+        <Input
+          type="password"
+          id="password"
+          placeholder="Sua senha"
+          {...register("password", { required: true, minLength: 10 })}
+        />
+        {errors.password?.type === "required" && (
+          <Text color="red.500" fontSize="sm">
+            Este campo é obrigatório
+          </Text>
+        )}
+        {errors.password?.type === "minLength" && (
+          <Text color="red.500" fontSize="sm">
+            Senha deve conter 10 caracteres
+          </Text>
+        )}
 
-          <FormLabel mt="1rem">Profissões</FormLabel>
-          <Select
-            defaultValue="0"
-            {...register("profession", { validate: (value) => value !== "0" })}
+        <FormLabel mt="1rem">Profissões</FormLabel>
+        <Select
+          defaultValue="0"
+          {...register("profession", { validate: (value) => value !== "0" })}
+        >
+          <option value="0">Selecione sua profissão...</option>
+          <option value="Desenvolvedor">Desenvolvedor</option>
+          <option value="Agilista">Agilista</option>
+          <option value="QA">QA</option>
+          <option value="Product Ower">Product Ower</option>
+          <option value="Tech Leader">Tech Leader</option>
+          <option value="Analista SEO">Analista SEO</option>
+          <option value="Outros">Outros</option>
+        </Select>
+        {errors.profession?.type === "validate" && (
+          <Text color="red.500" fontSize="sm">
+            Este campo é obrigatório
+          </Text>
+        )}
+        <Checkbox
+          mt="1rem"
+          {...register("privacyTerms", {
+            validate: (value) => value === true,
+          })}
+        >
+          <Text fontSize={["smaller", "sm", "medium"]}>
+            Eu concordo com os termos de privacidade
+          </Text>
+        </Checkbox>
+        {errors.privacyTerms?.type === "validate" && (
+          <Text color="red.500" fontSize="sm">
+            Você deve aceitar os termos de privacidade
+          </Text>
+        )}
+        <Center mt="1.5rem">
+          <Button
+            colorScheme="blue"
+            w="100%"
+            onClick={() => handleSubmit(OnSubmit)()}
+            isLoading={isSubmitting}
           >
-            <option value="0">Selecione sua profissão...</option>
-            <option value="Desenvolvedor">Desenvolvedor</option>
-            <option value="Agilista">Agilista</option>
-            <option value="QA">QA</option>
-            <option value="Product Ower">Product Ower</option>
-            <option value="Tech Leader">Tech Leader</option>
-            <option value="Analista SEO">Analista SEO</option>
-            <option value="Outros">Outros</option>
-          </Select>
-          {errors.profession?.type === "validate" && (
-            <Text color="red.500" fontSize="sm">
-              Este campo é obrigatório
-            </Text>
-          )}
-          <Checkbox
-            mt="1rem"
-            {...register("privacyTerms", {
-              validate: (value) => value === true,
-            })}
-          >
-            <Text fontSize={["smaller", "sm", "medium"]}>
-              Eu concordo com os termos de privacidade
-            </Text>
-          </Checkbox>
-          {errors.privacyTerms?.type === "validate" && (
-            <Text color="red.500" fontSize="sm">
-              Você deve aceitar os termos de privacidade
-            </Text>
-          )}
-          <Center mt="1.5rem">
-            <Button
-              colorScheme="blue"
-              w="100%"
-              onClick={() => handleSubmit(OnSubmit)()}
-              isLoading={isSubmitting}
-            >
-              Criar conta
-            </Button>
-          </Center>
-        </FormControl>
-      </Flex>
-    );
+            Criar conta
+          </Button>
+        </Center>
+      </FormControl>
+    </Flex>
+  );
 }
