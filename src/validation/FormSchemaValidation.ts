de6bf11dcdbd5935c205090bd0b4df4ddd5d64a2
Paulo Moutinho <paulo_vicali@icloud.com>
2023-01-09 19:42:10 -0300

    feat: validando campos com mensagem de erro padrão

--- src/validation/FormSchemaValidation.ts
+++ src/validation/FormSchemaValidation.ts
@@ -1,12 +1,23 @@
 import * as yup from "yup";
 
 export const FormCreateSchema = yup.object().shape({
-  name: yup.string().required("Nome é obrigatório"),
-  email: yup.string().required("E-mail é obrigatório").email("E-mail inválido"),
+  name: yup.string().required("Nome é obrigatório."),
+  email: yup
+    .string()
+    .required("E-mail é obrigatório.")
+    .email("E-mail inválido."),
   password: yup
     .string()
-    .required("Senha é obrigatório")
-    .min(10, "Senha deve conter mínimo de 10 caracteres"),
-  profession: yup.string().test((value) => value !== "0"),
-  privacyTerms: yup.boolean().test((value) => value === true)
+    .required("Senha é obrigatório.")
+    .min(10, "Senha deve conter mínimo de 10 caracteres."),
+  profession: yup
+    .string()
+    .test("profession", "Selecione uma profissão.", (value) => value !== "0"),
+  privacyTerms: yup
+    .boolean()
+    .test(
+      "privacyTerms",
+      "Você deve concordar com os termos.",
+      (value) => value === true
+    ),
 });
